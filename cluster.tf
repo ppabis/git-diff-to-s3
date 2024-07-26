4de09b84bfb26d56b79f4979d51a666c2fe7552c
ppabis <piotr@pabis.eu>
2024-07-26 20:17:33 +0200

    Add modules ecr and iam

--- cluster.tf
+++ cluster.tf
@@ -2,6 +2,16 @@ module "vpc" {
   source = "./vpc"
 }
 
+module "ecr" {
+  source = "./ecr"
+}
+
+module "iam" {
+  source              = "./iam"
+  codecommit_repo_arn = aws_codecommit_repository.CodeCommitRepo.arn
+  ecr_repository_arn  = module.ecr.repository_arn
+}
+
 resource "aws_ecs_cluster" "cluster" {
   name = "GitDiffCluster"
 }
